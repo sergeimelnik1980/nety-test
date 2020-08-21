e008a81f51c09f5158ee66e3d2435b1e4c370f49
spawn <sergey.melnik@valor-software.com>
2020-08-21 11:33:59 +0300

    10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app NOT!!!! works!';
+  title = 'app NOT faking works!';
 }
